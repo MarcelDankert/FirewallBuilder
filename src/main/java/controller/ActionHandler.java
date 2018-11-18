b694486bdb451c5f23886dc1b9b2bd7b553f4b88
Marcel Dankert <Marcel Dankert@MCL-DESKTOP-HOME>
2018-11-18 13:11:18 +0100

    *

--- src/main/java/controller/ActionHandler.java
+++ src/main/java/controller/ActionHandler.java
@@ -2,33 +2,48 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.swing.JOptionPane;
 
+import model.FileBuilder;
 import view.MyGui;
 
 public class ActionHandler implements ActionListener {
 	private MyGui mf;
-	
+	private FileBuilder fb;
 
 	public ActionHandler(MyGui mainFrame) {
 		this.mf = mainFrame;
 	}
 
-
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource()==mf.getSaveBtn()) {
+		if (e.getSource() == mf.getSaveBtn()) {
 			if (mf.getRegelNameTf().getText().isEmpty()) {
-			JOptionPane.showMessageDialog(null, "Bitte überprüfen Sie, ob alle Felder korrekt ausgefüllt sind.", "Fehlende Eingaben", JOptionPane.INFORMATION_MESSAGE); 
-			}
-			else {
+				JOptionPane.showMessageDialog(null, "Bitte überprüfen Sie, ob alle Felder korrekt ausgefüllt sind.",
+						"Fehlende Eingaben", JOptionPane.INFORMATION_MESSAGE);
+			} else {
 				mf.getAusgabeArea().setText("#" + mf.getRegelNameTf().getText());
+				try (BufferedWriter writer = new BufferedWriter(new FileWriter("firewall.sh"))) {
+					try {
+						writer.write(mf.getRegelNameTf().getText());
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					writer.close();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 			}
 		}
-		if (e.getSource()==mf.getExitBtn()) {
+		if (e.getSource() == mf.getExitBtn()) {
 			System.exit(0);
 		}
-		
+
 	}
 
 }
--- src/main/java/starter/MainExec.java
+++ src/main/java/starter/MainExec.java
@@ -1,9 +1,15 @@
 package starter;
 
+import java.io.File;
+
+import model.FileBuilder;
 import view.MyGui;
 
 public class MainExec {
-public static void main(String[] args) {
-	MyGui gui = new MyGui();
-}
+	public static void main(String[] args) {
+		FileBuilder fileBuilder = new FileBuilder();
+		String dateiName = "firewall.sh";
+		fileBuilder.createFile(new File(dateiName));
+		MyGui gui = new MyGui();
+	}
 }
